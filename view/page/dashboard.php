60f5f7bcba5244f03f87e14bd37c4ac25fa102fc
gilang <gilang.sukma@digitalsekuriti.id>
2024-06-16 12:24:29 +0700

    remove graph

--- view/page/dashboard.php
+++ view/page/dashboard.php
@@ -31,12 +31,9 @@ $dashboard_data = getDashboardData()
                         <div class="col-md-12 grid-margin">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div>
-                                    <h4 class="font-weight-bold mb-0">RoyalUI Dashboard</h4>
+                                    <h4 class="font-weight-bold mb-0">Dashboard Kas Mushola Rahmatullah</h4>
                                 </div>
                                 <div>
-                                    <button type="button" class="btn btn-primary btn-icon-text btn-rounded">
-                                        <i class="ti-clipboard btn-icon-prepend"></i>Report
-                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -91,34 +88,7 @@ $dashboard_data = getDashboardData()
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12 grid-margin stretch-card">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-title">Grafik Kas</p>
-                                    <p class="text-muted font-weight-light">Received overcame oh sensible so at an. Formed do change merely to county it. Am separate contempt domestic to to oh. On relation my so addition branched.</p>
-                                    <div id="sales-legend" class="chartjs-legend mt-4 mb-2"></div>
-                                    <canvas id="sales-chart"></canvas>
-                                </div>
-                                <div class="card border-right-0 border-left-0 border-bottom-0">
-                                    <div class="d-flex justify-content-center justify-content-md-end">
-                                        <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                                            <button class="btn btn-lg btn-outline-light dropdown-toggle rounded-0 border-top-0 border-bottom-0" type="button" id="dropdownMenuDate2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                                Today
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                                <a class="dropdown-item" href="#">January - March</a>
-                                                <a class="dropdown-item" href="#">March - June</a>
-                                                <a class="dropdown-item" href="#">June - August</a>
-                                                <a class="dropdown-item" href="#">August - November</a>
-                                            </div>
-                                        </div>
-                                        <button class="btn btn-lg btn-outline-light text-primary rounded-0 border-0 d-none d-md-block" type="button"> View all </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
@@ -136,77 +106,6 @@ $dashboard_data = getDashboardData()
 
     <?php include("../component/plugin.php"); ?>
 
-    <script>
-        $(document).ready(function() {
-            if ($("#sales-chart").length) {
-                var SalesChartCanvas = $("#sales-chart").get(0).getContext("2d");
-                var SalesChart = new Chart(SalesChartCanvas, {
-                    type: 'bar',
-                    data: {
-                        labels: [<?= json_encode($dashboard_data['graph'][4]['month_name']) ?>, <?= json_encode($dashboard_data['graph'][3]['month_name']) ?>, <?= json_encode($dashboard_data['graph'][2]['month_name']) ?>, <?= json_encode($dashboard_data['graph'][1]['month_name']) ?>, <?= json_encode($dashboard_data['graph'][0]['month_name']) ?>],
-                        datasets: [{
-                                label: 'Pengeluaran',
-                                data: [<?= json_encode($dashboard_data['graph'][4]['total_kaskeluar']) ?>, <?= json_encode($dashboard_data['graph'][3]['total_kaskeluar']) ?>, <?= json_encode($dashboard_data['graph'][2]['total_kaskeluar']) ?>, <?= json_encode($dashboard_data['graph'][1]['total_kaskeluar']) ?>, <?= json_encode($dashboard_data['graph'][0]['total_kaskeluar']) ?>],
-                                backgroundColor: '#8EB0FF'
-                            },
-                            {
-                                label: 'Pemasukan',
-                                data: [<?= json_encode($dashboard_data['graph'][4]['total_kasmasuk']) ?>, <?= json_encode($dashboard_data['graph'][3]['total_kasmasuk']) ?>, <?= json_encode($dashboard_data['graph'][2]['total_kasmasuk']) ?>, <?= json_encode($dashboard_data['graph'][1]['total_kasmasuk']) ?>, <?= json_encode($dashboard_data['graph'][0]['total_kasmasuk']) ?>],
-                                backgroundColor: '#316FFF'
-                            }
-                        ]
-                    },
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: true,
-                        layout: {
-                            padding: {
-                                left: 0,
-                                right: 0,
-                                top: 20,
-                                bottom: 0
-                            }
-                        },
-                        scales: {
-                            yAxes: [{
-                                display: true,
-                                gridLines: {
-                                    display: false,
-                                    drawBorder: false
-                                },
-                                ticks: {
-                                    display: false,
-                                    min: 0,
-                                    max: 1000000
-                                }
-                            }],
-                            xAxes: [{
-                                stacked: false,
-                                ticks: {
-                                    beginAtZero: true,
-                                    fontColor: "#9fa0a2"
-                                },
-                                gridLines: {
-                                    color: "rgba(0, 0, 0, 0)",
-                                    display: false
-                                },
-                                barPercentage: 1
-                            }]
-                        },
-                        legend: {
-                            display: false
-                        },
-                        elements: {
-                            point: {
-                                radius: 0
-                            }
-                        }
-                    },
-                });
-                document.getElementById('sales-legend').innerHTML = SalesChart.generateLegend();
-            }
-        });
-    </script>
 </body>
 
 </html>
