702ed1dcf5950b8918a7ad9d6734b1ee8248ac5c
gilang <gilang.sukma@digitalsekuriti.id>
2024-06-21 14:03:25 +0700

    add update user

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -30,6 +30,28 @@ function addUser($data)
     }
 }
 
+function updateUser($id, $data)
+{
+    global $pdo;
+
+    $hashed_password = password_hash($data['password'], PASSWORD_DEFAULT);
+
+    //update donasi
+    $query = "UPDATE user
+    SET nama = ?, username = ?, password = ?, usertype = ?
+    WHERE id_user = ?;";
+
+    try {
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$data['nama'], $data['username'], $hashed_password, $data['usertype'], $id]);
+        header("Location: user.php");
+    } catch (PDOException $e) {
+        //error
+        return $e->getMessage();
+    }
+}
+
+
 function deleteUser($id)
 {
     global $pdo;
@@ -42,6 +64,24 @@ function deleteUser($id)
         $stmt = $pdo->prepare($query);
         $stmt->execute([$id]);
         header("Location: user.php");
+    } catch (PDOException $e) {
+        //error
+        return $e->getMessage();
+    }
+}
+
+
+function getUserDetail($id)
+{
+    global $pdo;
+
+    $query = "SELECT * FROM user WHERE id_user = $id;";
+
+    try {
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $user = $stmt->fetch();
+        return $user;
     } catch (PDOException $e) {
         //error
         return $e->getMessage();
